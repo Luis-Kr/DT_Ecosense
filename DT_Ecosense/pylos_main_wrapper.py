21696a6faeed96edf84dcfdb6da699c2588b4019
Luis-Kr <luis1.kremer@gmail.com>
2024-09-16 12:37:49 +0000

    add new cameras

--- DT_Ecosense/pylos_main_wrapper.py
+++ DT_Ecosense/pylos_main_wrapper.py
@@ -17,12 +17,16 @@ if __name__ == "__main__":
                     "G5Bullet_32", "G5Bullet_34", "G5Bullet_36", "G5Bullet_39", 
                     "G5Bullet_40", "G5Bullet_43"]
     
+    # camera_names = ["G5Bullet_07", "G5Bullet_26", "G5Bullet_27", "G5Bullet_28", "G5Bullet_31", 
+    #                 "G5Bullet_32", "G5Bullet_33", "G5Bullet_34", "G5Bullet_35", "G5Bullet_36", 
+    #                 "G5Bullet_37", "G5Bullet_39", "G5Bullet_40", "G5Bullet_43", "G5Bullet_47", 
+    #                 "G5Bullet_48"]
+    
     #dates = [(2024, 8, 31), (2024, 8, 30), (2024, 8, 29), (2024, 8, 28)]
-    dates = [(2024, 8, 26), (2024, 8, 25), (2024, 8, 24), (2024, 8, 23), (2024, 9, 4)]
+    #dates = [(2024, 8, 26), (2024, 8, 25), (2024, 8, 24), (2024, 8, 23), (2024, 9, 4)]
+    dates = [(2024, 9, 3), (2024, 9, 4), (2024, 9, 5), (2024, 9, 6), (2024, 9, 7)]
     
     args = sorted(list(product(dates, camera_names)))
-    
-    print(args)
 
     # Set the multiprocessing start method to 'spawn'
     mp.set_start_method('spawn')
@@ -30,3 +34,7 @@ if __name__ == "__main__":
     # Create a pool of workers
     with mp.Pool(1) as pool: #len(camera_names)
         pool.map(run_main, args)
+        
+
+# New cameras
+#"G5Bullet_28", "G5Bullet_33", "G5Bullet_35", "G5Bullet_37", "G5Bullet_47", "G5Bullet_48"
