174727b040f8b4796ef51dc835e6a927a8142bb8
Luis-Kr <luis1.kremer@gmail.com>
2024-09-26 10:49:56 +0200

    Refactor get_previous_day() function to accept a specific day as an argument

--- DT_Ecosense/utils/VM/VM_wrapper.py
+++ DT_Ecosense/utils/VM/VM_wrapper.py
@@ -4,8 +4,9 @@ from itertools import product
 import pprint
 from datetime import datetime, timedelta
 
-def get_previous_day():
-    previous_date = datetime.now() - timedelta(days=1)
+def get_previous_day(day):
+    #previous_date = datetime.now() - timedelta(days=1)
+    previous_date = datetime(2024, 9, day)
     return (previous_date.year, previous_date.month, previous_date.day)
 
 def run_main(date, camera_name):
@@ -24,7 +25,7 @@ if __name__ == "__main__":
                      "G5Bullet_37", "G5Bullet_39", "G5Bullet_40", "G5Bullet_43", "G5Bullet_47", 
                      "G5Bullet_48"]
     
-    dates = [get_previous_day()]
+    dates = [get_previous_day(day=i) for i in list([16,17,18])]
     
     args = list(product(dates, camera_names))
         
