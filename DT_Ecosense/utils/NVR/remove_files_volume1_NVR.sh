5372c48b1c50a08ad85e82a0b3b1f1f30dac6c5e
Luis-Kr <luis1.kremer@gmail.com>
2024-09-26 10:51:18 +0200

    add new cameras

--- DT_Ecosense/utils/NVR/remove_files_volume1_NVR.sh
+++ DT_Ecosense/utils/NVR/remove_files_volume1_NVR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Date
-DATE="2024-09-14"
+DATE="2024-09-23"
 
 # Base directory
 BASE_DIR="/volume1/videos"
@@ -12,13 +12,19 @@ DIRS=(
     "G5Bullet_07"
     "G5Bullet_26"
     "G5Bullet_27"
+    "G5Bullet_28"
     "G5Bullet_31"
     "G5Bullet_32"
+    "G5Bullet_33"
     "G5Bullet_34"
+    "G5Bullet_35"
     "G5Bullet_36"
+    "G5Bullet_37"
     "G5Bullet_39"
     "G5Bullet_40"
     "G5Bullet_43"
+    "G5Bullet_47"
+    "G5Bullet_48"
 )
 
 # Loop through each camera and remove the files
